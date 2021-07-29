b8c7bea35e448efb6c0206089d640708715ae7af
cd ~/code/maaferna/dotfiles && zsh git_setup.sh <maaferna@gmail.com>
2021-07-29 17:44:36 -0400

    add show - falta edit-replace

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do
+  Article.create([{title: Faker::Name.name, content: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 50) }])
+end
